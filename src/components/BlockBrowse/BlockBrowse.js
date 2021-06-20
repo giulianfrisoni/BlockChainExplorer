9cb5b730005873eaabfe5adaae4dcb0a8aaa9ddf
giulianfrisoni <giulianfrisoni@gmail.com>
2021-06-19 20:49:28 -0600

    v1.0.1

--- src/components/BlockBrowse/BlockBrowse.js
+++ src/components/BlockBrowse/BlockBrowse.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import Table from '@material-ui/core/Table';
 import TableContainer from '@material-ui/core/TableContainer';
 import Button from '@material-ui/core/Button';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import { Link } from 'react-router-dom'
 import { Typography } from '@material-ui/core';
 import { Box } from '@material-ui/core';
+import { DataGrid } from '@material-ui/data-grid';
 /* INFURA NODE */
-/* var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/ef64c86f37a84cc9a7929ba567d8138d')); */
+/*var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/ef64c86f37a84cc9a7929ba567d8138d'));*/
 /* GANACHE LOCAL NODE */
 var web3 = new Web3(new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545'));
 
@@ -24,6 +19,7 @@ class BlockBrowse extends Component {
             block_ids: [],
             curr_block_no: web3.eth.blockNumber,
             block_data: [],
+            block_data2: [],
             button_state: {
                 text: "Stop Refresh",
                 color: "default",
@@ -35,6 +31,7 @@ class BlockBrowse extends Component {
         if (block_no !== "") {
             const block_idno = this.state.block_ids.slice();
             const block_datainfo = this.state.block_data.slice();
+            const block_datainfo2 = this.state.block_data2.slice();
             var max_blocks = 10;
             if (block_no < max_blocks) max_blocks = block_no;
             for (var i = 0; i < max_blocks; i++, block_no--) {
@@ -48,23 +45,30 @@ class BlockBrowse extends Component {
                         difficulty: parseInt(currBlockObj.difficulty),
                         hash: (web3.eth.getBlock(currBlockObj.number).hash)
                     })
-                    console.log(block_datainfo);
+                    block_datainfo2.push({
+                        id: currBlockObj.number,
+                        transno: web3.eth.getBlockTransactionCount(currBlockObj.number),
+                        miner: currBlockObj.miner,
+                        difficulty: parseInt(currBlockObj.difficulty),
+                        hash:(web3.eth.getBlock(currBlockObj.number).hash)
+                    })
                 }
             }
             this.setState({
                 block_ids: block_idno,
                 block_data: block_datainfo,
+                block_data2: block_datainfo2,
                 curr_block_no: block_no
             })
         }
     }
-    componentDidMount() {
+    componentWillMount() {
         this.getBlocks(this.curr_block_no);
         this.refreshinterval = setInterval(this.getBlocks.bind(this), 5000); // runs every 5 seconds.
 
     }
     componentWillUnmount() {
-        clearInterval(this.getBlocks);
+        clearInterval(this.refreshinterval);
     }
     handleClickRow(event, id) {
 
@@ -79,7 +83,7 @@ class BlockBrowse extends Component {
                 }
             })
 
-            clearInterval(this.getBlocks);
+            clearInterval(this.refreshinterval);
         } else {
             this.setState({
                 button_state: {
@@ -91,47 +95,46 @@ class BlockBrowse extends Component {
     }
 
     render() {
+        const columns = [
+            { field: 'id', headerName: 'Block Number', flex: 0.1 },
+            { field: 'transno', headerName: 'Number of Transactions', flex: 0.1 },
+            { field: 'miner', headerName: 'Miner', flex: 0.3 },
+            { field: 'difficulty', headerName: 'Difficulty', flex: 0.25 },
+            {
+                field: 'hash', headerName: 'Hash', flex: 0.25, 
+                renderCell: (params) => (
+                    <Link to={`/blocktable/${params.value}`}>{params.value}</Link>
+                )},
+                
+        ];
         return (
             <div className="Home">
-                <Paper >
-                    <Box alignItems="center">
-                        <Typography variant="h2">Home page</Typography>
+                <Box alignContent="flex-end" m={1}>
+                    <Box pt={3}>
+                    <Typography variant="h2">Home page</Typography>
                     </Box>
-                    <Box alignContent="flex-end">
-                        <Button variant="contained" color={this.state.button_state.color} onClick={() => this.handleClickButton()}>
-                            {this.state.button_state.text}
-                        </Button>
+                    <Box pt={1}>
+                    <Typography variant="h2">Currently Working with:Ganache at HTTP://127.0.0.1:7545</Typography>
                     </Box>
-                    <Box>
-                        <TableContainer >
-                            <Table size="medium" aria-label="a dense table">
-                                <TableHead>
-
-                                    <TableRow>
-                                        <TableCell sortDirection="desc" >Block #</TableCell>
-                                        <TableCell align="right"  >Transaction #</TableCell>
-                                        <TableCell align="right">Miner</TableCell>
-                                        <TableCell align="right">Hash Difficulty</TableCell>
-                                        <TableCell align="right" >Hash ID</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {this.state.block_data.map((row) => (
-                                        <TableRow key={row.key} >
-                                            <TableCell component="th" scope="row" filtering="true" sorting="true" onClick={() => this.handleClickRow(row.hash)}>
-                                                {row.key}
-                                            </TableCell>
-                                            <TableCell align="right">{row.transno}</TableCell>
-                                            <TableCell align="right">{row.miner}</TableCell>
-                                            <TableCell align="right">{row.difficulty}</TableCell>
-                                            <TableCell align="right" component={Link} to={`/blocktable/${row.hash.toString()}/`} key={row.key} >{row.hash}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
+                    <Box pt={3}>
+                    <Button variant="contained" color={this.state.button_state.color} onClick={() => this.handleClickButton()}>
+                        {this.state.button_state.text}
+                    </Button>
                     </Box>
-                </Paper>
+                    <Box pt={3}>
+                    <TableContainer >
+                        <div style={{ height: 500, width: '100%' }}>
+                            <DataGrid
+                                rows={this.state.block_data2}
+                                columns={columns}
+                                pageSize={10}
+                                checkboxSelection
+                                disableSelectionOnClick
+                            />
+                        </div>
+                    </TableContainer>
+                    </Box>
+                </Box>
             </div>
         );
     }
--- src/components/BlockTable/BlockTable.js
+++ src/components/BlockTable/BlockTable.js
@@ -11,8 +11,9 @@ import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
 import { Typography } from '@material-ui/core';
 import Web3 from 'web3';
+import { DataGrid } from '@material-ui/data-grid';
 /* INFURA NODE */
-/* var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/ef64c86f37a84cc9a7929ba567d8138d')); */
+/*var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/ef64c86f37a84cc9a7929ba567d8138d'));*/
 /* GANACHE LOCAL NODE */
 var web3 = new Web3(new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545'));
 
@@ -21,7 +22,8 @@ class BlockTable extends Component {
     super(props);
     this.state = {
       blocktable: [],
-      block_hash: props.match.params.blockHash
+      block_hash: props.match.params.blockHash,
+      block_transactions: []
     }
   }
   componentWillMount() {
@@ -30,6 +32,7 @@ class BlockTable extends Component {
   }
   getBlockState(block_hid) {
     var currBlockObj = web3.eth.getBlock(this.block_hash);
+
     // Set the Component state
     this.setState({
       block_id: currBlockObj.number,
@@ -37,6 +40,25 @@ class BlockTable extends Component {
       block_ts: Date(parseInt(this.state.blocktable.timestamp, 10)).toString(),
       block_txs: parseInt(currBlockObj.transactions.slice().length, 10),
       block: currBlockObj
+    })
+    currBlockObj.transactions.forEach(element => {
+      this.getTransactionsState(element);
+    });
+
+  }
+  getTransactionsState(transaction_hid) {
+    var currHashObj = web3.eth.getTransaction(transaction_hid);
+    var cur_hash_table = this.state.block_transactions;
+    var gasineth = web3.fromWei(currHashObj.gasPrice.toNumber(), "ether" ) ;
+    var valueineth = web3.fromWei(currHashObj.value.toNumber(), "ether" );
+    cur_hash_table.push({
+      id: currHashObj.hash,
+      gas: gasineth,
+      value: valueineth,
+    })
+    // Set the Component state
+    this.setState({
+      block_transactions: cur_hash_table,
     })
   }
 
@@ -51,6 +73,12 @@ class BlockTable extends Component {
   }
 
   render() {
+    const columns = [
+      { field: 'id', headerName: 'Transaction Hash', flex: 0.5 },
+      { field: 'value', headerName: 'Amount', flex: 0.25 },
+      { field: 'gas', headerName: 'Gas amount' , flex: 0.25},
+
+    ];
     const block = this.state.block;
     return (
       <div className="Block">
@@ -80,10 +108,25 @@ class BlockTable extends Component {
                 </Table>
               </TableContainer>
             </Paper>
+            <Paper>
+              <Typography variant="h2">Viewing Data of  {this.state.block_txs} Transactions</Typography>
+              <TableContainer >
+                <div style={{ height: 400, width: '100%' }}>
+                  <DataGrid
+                    rows={this.state.block_transactions}
+                    columns={columns}
+                    pageSize={10}
+                    checkboxSelection
+                    disableSelectionOnClick
+
+                  />
+                </div>
+              </TableContainer>
+            </Paper>
           </Grid>
           <Paper>
             <Grid item xs={12}>
-              <Button size="large" variant="contained" color="black" component={Link} to={`/`}>
+              <Button size="large" variant="contained" color="primary" component={Link} to={`/`}>
                 Go Back
               </Button>
             </Grid>
